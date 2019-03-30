eb391139ab2f9b076901862eea4322769af7bd7f
Ryan Haley <ryanhaley@frazer.biz>
2019-03-30 00:05:36 -0400

    Add some initial tests and queries

---
+++ qb.go
@@ -0,0 +1,213 @@
+package qb
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+func In(field string) Query {
+	return InClause(field)
+}
+
+type InClause string
+
+func (c InClause) Build() string {
+	return fmt.Sprintf("%s IN (?)", string(c))
+}
+
+func (c InClause) String() string {
+	return c.Build()
+}
+
+type ComparisonClause struct {
+	Op    string
+	Field string
+	Value interface{}
+}
+
+func (c ComparisonClause) Build() string {
+	switch v := c.Value.(type) {
+	case string:
+		return fmt.Sprintf("%s %s '%s'", c.Field, c.Op, v)
+	case int:
+		return fmt.Sprintf("%s %s %d", c.Field, c.Op, v)
+	default:
+		return fmt.Sprintf("%s %s %s", c.Field, c.Op, c.Value)
+	}
+}
+
+func (c ComparisonClause) String() string {
+	return c.Build()
+}
+
+func Greater(field string, value interface{}) Query {
+	return ComparisonClause{
+		Op:    ">",
+		Field: field,
+		Value: value,
+	}
+}
+
+func GreaterEqual(field string, value interface{}) Query {
+	return ComparisonClause{
+		Op:    ">=",
+		Field: field,
+		Value: value,
+	}
+}
+
+func Less(field string, value interface{}) Query {
+	return ComparisonClause{
+		Op:    "<",
+		Field: field,
+		Value: value,
+	}
+}
+
+func LessEqual(field string, value interface{}) Query {
+	return ComparisonClause{
+		Op:    "<=",
+		Field: field,
+		Value: value,
+	}
+}
+
+func Equal(field string, value interface{}) Query {
+	return ComparisonClause{
+		Op:    "=",
+		Field: field,
+		Value: value,
+	}
+}
+
+func Or(comp1, comp2 Query) Query {
+	return BooleanQuery{
+		Op:          "OR",
+		Comparison1: comp1,
+		Comparison2: comp2,
+	}
+}
+
+func And(comp1, comp2 Query) Query {
+	return BooleanQuery{
+		Op:          "AND",
+		Comparison1: comp1,
+		Comparison2: comp2,
+	}
+}
+
+type BooleanQuery struct {
+	Op          string
+	Comparison1 Query
+	Comparison2 Query
+}
+
+func (q BooleanQuery) Build() string {
+	return fmt.Sprintf("%s %s %s", q.Comparison1.Build(), q.Op, q.Comparison2.Build())
+}
+
+func (q BooleanQuery) String() string {
+	return q.Build()
+}
+
+type Query interface {
+	fmt.Stringer
+	Build() string
+}
+
+type SelectQuery struct {
+	Table       string
+	Fields      []string
+	WhereClause Query
+}
+
+func (q SelectQuery) Where(wq Query) Query {
+	q.WhereClause = wq
+	return q
+}
+
+func (q SelectQuery) Build() string {
+	var stmt string
+	if len(q.Fields) == 0 {
+		stmt = fmt.Sprintf("SELECT * FROM %s", q.Table)
+	} else {
+		fields := strings.Join(q.Fields, ", ")
+		stmt = fmt.Sprintf("SELECT %s FROM %s", fields, q.Table)
+	}
+	if q.WhereClause != nil {
+		stmt += fmt.Sprintf(" WHERE %s", q.WhereClause.Build())
+	}
+	return stmt + ";"
+}
+
+func (q SelectQuery) String() string {
+	b, err := json.MarshalIndent(q, "", "    ")
+	if err != nil {
+		return ""
+	}
+	return string(b)
+}
+
+func Select(table string, fields ...string) SelectQuery {
+	return SelectQuery{
+		Table:  table,
+		Fields: fields,
+	}
+}
+
+func Join(sq1, sq2 SelectQuery) JoinQuery {
+	return JoinQuery{
+		Query1: sq1,
+		Query2: sq2,
+	}
+}
+
+type On struct {
+	Field1 string
+	Field2 string
+}
+
+func (o On) Build() string {
+	return fmt.Sprintf("%s = %s", o.Field1, o.Field2)
+}
+
+func (o On) String() string {
+	return o.Build()
+}
+
+type JoinQuery struct {
+	Query1   SelectQuery
+	Query2   SelectQuery
+	OnClause Query
+}
+
+func (q JoinQuery) Build() string {
+	fields := make([]string, 0)
+	for _, field := range q.Query1.Fields {
+		fields = append(fields, q.Query1.Table+"."+field)
+	}
+	for _, field := range q.Query2.Fields {
+		fields = append(fields, q.Query2.Table+"."+field)
+	}
+
+	stmt := fmt.Sprintf("SELECT %s FROM %s, %s", strings.Join(fields, ", "), q.Query1.Table, q.Query2.Table)
+	stmt += fmt.Sprintf(" WHERE %s", q.OnClause.Build())
+	return stmt + ";"
+}
+
+func (q JoinQuery) String() string {
+	b, err := json.MarshalIndent(q, "", "    ")
+	if err != nil {
+		return ""
+	}
+	return string(b)
+}
+
+func (q JoinQuery) On(field1, field2 string) JoinQuery {
+	q.OnClause = On{
+		Field1: field1,
+		Field2: field2,
+	}
+	return q
+}
---
+++ qb_test.go
@@ -0,0 +1,116 @@
+package qb_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/haleyrc/qb"
+)
+
+func TestQuery(t *testing.T) {
+	type testcase struct {
+		name  string
+		query qb.Query
+		want  string
+	}
+	testcases := []testcase{
+		testcase{
+			name:  "simple query",
+			query: qb.Select("dealerships"),
+			want:  `SELECT * FROM dealerships;`,
+		},
+		testcase{
+			name:  "simple query with fields",
+			query: qb.Select("dealerships", "id", "name"),
+			want:  `SELECT id, name FROM dealerships;`,
+		},
+		testcase{
+			name: "simple query with fields and where",
+			query: qb.
+				Select("dealerships", "id", "name").
+				Where(qb.Equal("state", "NY")),
+			want: `SELECT id, name FROM dealerships WHERE state = 'NY';`,
+		},
+		testcase{
+			name: "simple query with greater than",
+			query: qb.
+				Select("vehicles", "id").
+				Where(qb.Greater("cost", 10)),
+			want: `SELECT id FROM vehicles WHERE cost > 10;`,
+		},
+		testcase{
+			name: "simple query with greater or equal",
+			query: qb.
+				Select("vehicles", "id").
+				Where(qb.GreaterEqual("cost", 10)),
+			want: `SELECT id FROM vehicles WHERE cost >= 10;`,
+		},
+		testcase{
+			name: "simple query with less than",
+			query: qb.
+				Select("vehicles", "id").
+				Where(qb.Less("cost", 10)),
+			want: `SELECT id FROM vehicles WHERE cost < 10;`,
+		},
+		testcase{
+			name: "simple query with less or equal",
+			query: qb.
+				Select("vehicles", "id").
+				Where(qb.LessEqual("cost", 10)),
+			want: `SELECT id FROM vehicles WHERE cost <= 10;`,
+		},
+		testcase{
+			name: "simple query with and",
+			query: qb.
+				Select("vehicles", "id").
+				Where(qb.And(
+					qb.Greater("cost", 10),
+					qb.Less("dol", 3),
+				)),
+			want: `SELECT id FROM vehicles WHERE cost > 10 AND dol < 3;`,
+		},
+		testcase{
+			name: "simple query with or",
+			query: qb.
+				Select("vehicles", "id").
+				Where(qb.Or(
+					qb.Greater("cost", 10),
+					qb.Less("dol", 3),
+				)),
+			want: `SELECT id FROM vehicles WHERE cost > 10 OR dol < 3;`,
+		},
+		testcase{
+			name: "simple query with in",
+			query: qb.
+				Select("vehicles", "id").
+				Where(qb.In("make")),
+			want: `SELECT id FROM vehicles WHERE make IN (?);`,
+		},
+		testcase{
+			name: "join query",
+			query: qb.Join(
+				qb.Select("employees", "id", "role"),
+				qb.Select("dealerships", "name"),
+			).On("employees.dealership_id", "dealerships.id"),
+			want: `SELECT employees.id, employees.role, dealerships.name FROM employees, dealerships WHERE employees.dealership_id = dealerships.id;`,
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.query.Build()
+			if got != tc.want {
+				t.Errorf("built:\n%s\nwanted:\n%s\ngot:\n%s", indent(tc.query), tc.want, got)
+			}
+		})
+	}
+}
+
+func indent(stringer fmt.Stringer) string {
+	s := stringer.String()
+	lines := strings.Split(s, "\n")
+	for i := range lines {
+		lines[i] = "\t" + lines[i]
+	}
+	return strings.Join(lines, "\n")
+}
